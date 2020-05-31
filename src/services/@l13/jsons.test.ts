0ca44cce1ff66159e22e55af01e68f10b9d51498
L13 <L13.RARY@gmail.com>
2020-05-31 18:01:57 +0200

    - restructured unit tests for jsons.parse

--- src/services/@l13/jsons.test.ts
+++ src/services/@l13/jsons.test.ts
@@ -9,430 +9,510 @@ type Test = {
 };
 
 describe('jsons', () => {
-	
+
 	describe('.parse()', () => {
-		
-		const tests:Test[] = [
-			{
-				desc: 'no comment',
-				expect: `{
-					"a": 1
-				}`,
-				toBe: { a: 1 },
-			},
-			{
-				desc: 'no comment',
-				expect: `{
-					"a": "1"
-				}`,
-				toBe: { a: '1' },
-			},
-			{
-				desc: 'remove single line comment at document atart',
-				expect: `// test
+
+		function runTests (tests:Test[]) {
+
+			for (const test of tests) {
+				it(test.desc, () => assert.deepEqual(parse(test.expect), test.toBe));
+			}
+
+		}
+
+		describe('no comments', () => {
+
+			runTests([
 				{
-					"a": 1
-				}
-				`,
-				toBe: { a: 1 },
-			},
-			{
-				desc: 'remove single line comment at object start',
-				expect: `
-				{ // test
-					"a": 1
-				}
-				`,
-				toBe: { a: 1 },
-			},
-			{
-				desc: 'remove single line comment after member',
-				expect: `
+					desc: 'with number value',
+					expect: `{
+						"a": 1
+					}`,
+					toBe: { a: 1 },
+				},
 				{
-					"a": 1 // test
-				}
-				`,
-				toBe: { a: 1 },
-			},
-			{
-				desc: 'remove single line comment at object end',
-				expect: `
+					desc: 'with string value',
+					expect: `{
+						"a": "1"
+					}`,
+					toBe: { a: '1' },
+				},
+			]);
+
+		});
+
+		describe('remove single line comment', () => {
+
+			runTests([
 				{
-					"a": 1
-				} // test
-				`,
-				toBe: { a: 1 },
-			},
-			{
-				desc: 'remove single line comment at document end',
-				expect: `
+					desc: 'at document atart',
+					expect: `// test
+					{
+						"a": 1
+					}
+					`,
+					toBe: { a: 1 },
+				},
 				{
-					"a": 1
-				}
-				// test`,
-				toBe: { a: 1 },
-			},
-			{
-				desc: 'remove all single line comments',
-				expect: `// test
-				{// test
-					"a": 1// test
-				}// test
-				// test`,
-				toBe: { a: 1 },
-			},
-			{
-				desc: 'remove multi line comment',
-				expect: `/* test */
+					desc: 'at object start',
+					expect: `
+					{ // test
+						"a": 1
+					}
+					`,
+					toBe: { a: 1 },
+				},
 				{
-					"a": 1
-				}
-				`,
-				toBe: { a: 1 },
-			},
-			{
-				desc: 'remove multi line comment at object start',
-				expect: `
-				{ /* test */
-					"a": 1
-				}
-				`,
-				toBe: { a: 1 },
-			},
-			{
-				desc: 'remove multi line comment before member',
-				expect: `
+					desc: 'after member',
+					expect: `
+					{
+						"a": 1 // test
+					}
+					`,
+					toBe: { a: 1 },
+				},
 				{
-					/* test */ "a": 1
-				}
-				`,
-				toBe: { a: 1 },
-			},
-			{
-				desc: 'remove multi line comment between member',
-				expect: `
+					desc: 'at object end',
+					expect: `
+					{
+						"a": 1
+					} // test
+					`,
+					toBe: { a: 1 },
+				},
 				{
-					"a":/* test */1
-				}
-				`,
-				toBe: { a: 1 },
-			},
-			{
-				desc: 'remove multi line comment after member',
-				expect: `
+					desc: 'at document end',
+					expect: `
+					{
+						"a": 1
+					}
+					// test`,
+					toBe: { a: 1 },
+				},
 				{
-					"a": 1 /* test */
-				}
-				`,
-				toBe: { a: 1 },
-			},
-			{
-				desc: 'remove multi line comment at object end',
-				expect: `
+					desc: 'remove all single line comments',
+					expect: `// test
+					{// test
+						"a": 1// test
+					}// test
+					// test`,
+					toBe: { a: 1 },
+				},
+			]);
+
+		});
+
+		describe('remove multi line comment', () => {
+
+			runTests([
 				{
-					"a": 1
-				} /* test */
-				`,
-				toBe: { a: 1 },
-			},
-			{
-				desc: 'remove multi line comment at documnt end',
-				expect: `
+					desc: 'at document start',
+					expect: `/* test */
+					{
+						"a": 1
+					}
+					`,
+					toBe: { a: 1 },
+				},
 				{
-					"a": 1
-				}
-				/* test */`,
-				toBe: { a: 1 },
-			},
-			{
-				desc: 'remove all multi line comments',
-				expect: `/* test */
-				{/* test */
-					/* test */"a":/* test */1/* test */
-				}/* test */
-				/* test */`,
-				toBe: { a: 1 },
-			},
-			{
-				desc: 'remove bigger multi line comment',
-				expect: `/*
-				test
-				*/{
-					"a": 1
-				}
-				`,
-				toBe: { a: 1 },
-			},
-			{
-				desc: 'remove bigger multi line comment at object start',
-				expect: `
-				{/*
+					desc: 'at object start',
+					expect: `
+					{ /* test */
+						"a": 1
+					}
+					`,
+					toBe: { a: 1 },
+				},
+				{
+					desc: 'before member',
+					expect: `
+					{
+						/* test */ "a": 1
+					}
+					`,
+					toBe: { a: 1 },
+				},
+				{
+					desc: 'between member',
+					expect: `
+					{
+						"a":/* test */1
+					}
+					`,
+					toBe: { a: 1 },
+				},
+				{
+					desc: 'after member',
+					expect: `
+					{
+						"a": 1 /* test */
+					}
+					`,
+					toBe: { a: 1 },
+				},
+				{
+					desc: 'at object end',
+					expect: `
+					{
+						"a": 1
+					} /* test */
+					`,
+					toBe: { a: 1 },
+				},
+				{
+					desc: 'at documnt end',
+					expect: `
+					{
+						"a": 1
+					}
+					/* test */`,
+					toBe: { a: 1 },
+				},
+				{
+					desc: 'remove all multi line comments',
+					expect: `/* test */
+					{/* test */
+						/* test */"a":/* test */1/* test */
+					}/* test */
+					/* test */`,
+					toBe: { a: 1 },
+				},
+			]);
+
+		});
+
+		describe('remove bigger multi line comments', () => {
+
+			runTests([
+				{
+					desc: 'at document start',
+					expect: `/*
 					test
-				*/
-					"a": 1
-				}
-				`,
-				toBe: { a: 1 },
-			},
-			{
-				desc: 'remove bigger multi line comment before member',
-				expect: `
+					*/{
+						"a": 1
+					}
+					`,
+					toBe: { a: 1 },
+				},
 				{
-					/*
-					test
-					*/"a": 1
-				}
-				`,
-				toBe: { a: 1 },
-			},
-			{
-				desc: 'remove bigger multi line comment between member',
-				expect: `
+					desc: 'at object start',
+					expect: `
+					{/*
+						test
+					*/
+						"a": 1
+					}
+					`,
+					toBe: { a: 1 },
+				},
 				{
-					"a":/*
-					test
-					*/1
-				}
-				`,
-				toBe: { a: 1 },
-			},
-			{
-				desc: 'remove bigger multi line comment after member',
-				expect: `
+					desc: 'before member',
+					expect: `
+					{
+						/*
+						test
+						*/"a": 1
+					}
+					`,
+					toBe: { a: 1 },
+				},
 				{
-					"a": 1 /*
+					desc: 'between member',
+					expect: `
+					{
+						"a":/*
+						test
+						*/1
+					}
+					`,
+					toBe: { a: 1 },
+				},
+				{
+					desc: 'after member',
+					expect: `
+					{
+						"a": 1 /*
+						test
+						*/
+					}
+					`,
+					toBe: { a: 1 },
+				},
+				{
+					desc: 'at object end',
+					expect: `
+					{
+						"a": 1
+					}/*
 					test
 					*/
-				}
-				`,
-				toBe: { a: 1 },
-			},
-			{
-				desc: 'remove bigger multi line comment at object end',
-				expect: `
+					`,
+					toBe: { a: 1 },
+				},
 				{
-					"a": 1
-				}/*
-				test
-				*/
-				`,
-				toBe: { a: 1 },
-			},
-			{
-				desc: 'remove bigger multi line comment at documnt end',
-				expect: `
-				{
-					"a": 1
-				}
-				/*
-					test
-				*/`,
-				toBe: { a: 1 },
-			},
-			{
-				desc: 'remove bigger all multi line comments',
-				expect: `/*
-				test
-				*/
-				{/*
-					test
-				*//*
-					test
-				*/"a":/*
-					test
-				*/1/*
-					test
-				*/
-				}/*
-					test
-				*//*
-					test
-				*/`,
-				toBe: { a: 1 },
-			},
-			{
-				desc: 'ignore single line comment inside multiline comment',
-				expect: `{
-					"a": 1 /* // */
-				}
-				`,
-				toBe: { 'a': 1 },
-			},
-			{
-				desc: 'ignore multil line comment start inside multiline comment',
-				expect: `{
-					"a": 1 /* /* */
-				}
-				`,
-				toBe: { 'a': 1 },
-			},
-			{
-				desc: 'ignore single line comment inside bigger multiline comment',
-				expect: `{
-					"a": 1 /*
-					//
-				*/
-				}
-				`,
-				toBe: { 'a': 1 },
-			},
-			{
-				desc: 'ignore multil line comment start inside bigger multiline comment',
-				expect: `{
-					"a": 1 /*
+					desc: 'at documnt end',
+					expect: `
+					{
+						"a": 1
+					}
 					/*
-				*/
-				}
-				`,
-				toBe: { 'a': 1 },
-			},
-			{
-				desc: 'ignore single line comment in key',
-				expect: `
+						test
+					*/`,
+					toBe: { a: 1 },
+				},
 				{
-					"a// test": "1"
-				}
-				`,
-				toBe: { 'a// test': '1' },
-			},
-			{
-				desc: 'ignore single line comment in value',
-				expect: `
+					desc: 'remove bigger all multi line comments',
+					expect: `/*
+					test
+					*/
+					{/*
+						test
+					*//*
+						test
+					*/"a":/*
+						test
+					*/1/*
+						test
+					*/
+					}/*
+						test
+					*//*
+						test
+					*/`,
+					toBe: { a: 1 },
+				},
+			]);
+
+		});
+
+		describe('ignnore single line comment inside', () => {
+
+			runTests([
 				{
-					"a": "1// test"
-				}
-				`,
-				toBe: { 'a': '1// test' },
-			},
-			{
-				desc: 'ignore multi line comment in key',
-				expect: `
+					desc: 'multi line comment',
+					expect: `{
+						"a": 1 /* // */
+					}
+					`,
+					toBe: { 'a': 1 },
+				},
 				{
-					"a/* test */": "1"
-				}
-				`,
-				toBe: { 'a/* test */': '1' },
-			},
-			{
-				desc: 'ignore multi line comment in value',
-				expect: `
+					desc: 'bigger multi line comment',
+					expect: `{
+						"a": 1 /*
+						//
+					*/
+					}
+					`,
+					toBe: { 'a': 1 },
+				},
+			]);
+
+		});
+
+		describe('multi line comment start inside', () => {
+
+			runTests([
 				{
-					"a": "1/* test */"
-				}
-				`,
-				toBe: { 'a': '1/* test */' },
-			},
-			{
-				desc: 'ignore double quote in key',
-				expect: `{
-					"\\"": 1
-				}`,
-				toBe: { '"': 1 },
-			},
-			{
-				desc: 'ignore double quote at key start',
-				expect: `{
-					"a\\"": 1
-				}`,
-				toBe: { 'a"': 1 },
-			},
-			{
-				desc: 'ignore double quote at key end',
-				expect: `{
-					"a\\"": 1
-				}`,
-				toBe: { 'a"': 1 },
-			},
-			{
-				desc: 'ignore double quote in key with string value',
-				expect: `{
-					"\\"": "1"
-				}`,
-				toBe: { '"': '1' },
-			},
-			{
-				desc: 'ignore double quote at key start with string value',
-				expect: `{
-					"\\"a": "1"
-				}`,
-				toBe: { '"a': '1' },
-			},
-			{
-				desc: 'ignore double quote at key end with string value',
-				expect: `{
-					"a\\"": "1"
-				}`,
-				toBe: { 'a"': '1' },
-			},
-			{
-				desc: 'ignore double quote in string value',
-				expect: `{
-					"a": "\\""
-				}`,
-				toBe: { 'a': '"' },
-			},
-			{
-				desc: 'ignore double quote at value start',
-				expect: `{
-					"a": "\\"1"
-				}`,
-				toBe: { 'a': '"1' },
-			},
-			{
-				desc: 'ignore double quote at value end',
-				expect: `{
-					"a": "1\\""
-				}`,
-				toBe: { 'a': '1"' },
-			},
-			{
-				desc: 'ignore double quotes in key and value',
-				expect: `{
-					"\\"": "\\""
-				}`,
-				toBe: { '"': '"' },
-			},
-			{
-				desc: 'ignore multiple double quotes in key and value',
-				expect: `{
-					"\\"\\"\\"": "\\"\\"\\""
-				}`,
-				toBe: { '"""': '"""' },
-			},
-			{
-				desc: 'ignore double quotes at key and value start',
-				expect: `{
-					"\\"a": "\\"1"
-				}`,
-				toBe: { '"a': '"1' },
-			},
-			{
-				desc: 'ignore multipe double quotes at key and value start',
-				expect: `{
-					"\\"\\"\\"a": "\\"\\"\\"1"
-				}`,
-				toBe: { '"""a': '"""1' },
-			},
-			{
-				desc: 'ignore double quotes at key and value end',
-				expect: `{
-					"a\\"": "1\\""
-				}`,
-				toBe: { 'a"': '1"' },
-			},
-			{
-				desc: 'ignore multipe double quotes at key and value end',
-				expect: `{
-					"a\\"\\"\\"": "1\\"\\"\\""
-				}`,
-				toBe: { 'a"""': '1"""' },
-			},
-		];
-		
-		for (const test of tests) {
-			it(test.desc, () => assert.deepEqual(parse(test.expect), test.toBe));
-		}
-		
+					desc: 'multi line comment',
+					expect: `{
+						"a": 1 /* /* */
+					}
+					`,
+					toBe: { 'a': 1 },
+				},
+				{
+					desc: 'bigger multi line comment',
+					expect: `{
+						"a": 1 /*
+						/*
+					*/
+					}
+					`,
+					toBe: { 'a': 1 },
+				},
+			]);
+
+		});
+
+		describe('ignore single line comment in', () => {
+
+			runTests([
+				{
+					desc: 'key',
+					expect: `
+					{
+						"a// test": "1"
+					}
+					`,
+					toBe: { 'a// test': '1' },
+				},
+				{
+					desc: 'string value',
+					expect: `
+					{
+						"a": "1// test"
+					}
+					`,
+					toBe: { 'a': '1// test' },
+				},
+			]);
+
+		});
+
+		describe('ignore multi line comment in string in', () => {
+
+			runTests([
+				{
+					desc: 'key',
+					expect: `
+					{
+						"a/* test */": "1"
+					}
+					`,
+					toBe: { 'a/* test */': '1' },
+				},
+				{
+					desc: 'string value',
+					expect: `
+					{
+						"a": "1/* test */"
+					}
+					`,
+					toBe: { 'a': '1/* test */' },
+				},
+			]);
+
+		});
+
+		describe('ignore double quote', () => {
+
+			runTests([
+				{
+					desc: 'in key',
+					expect: `{
+						"\\"": 1
+					}`,
+					toBe: { '"': 1 },
+				},
+				{
+					desc: 'at key start',
+					expect: `{
+						"a\\"": 1
+					}`,
+					toBe: { 'a"': 1 },
+				},
+				{
+					desc: 'at key end',
+					expect: `{
+						"a\\"": 1
+					}`,
+					toBe: { 'a"': 1 },
+				},
+				{
+					desc: 'in key with string value',
+					expect: `{
+						"\\"": "1"
+					}`,
+					toBe: { '"': '1' },
+				},
+				{
+					desc: 'at key start with string value',
+					expect: `{
+						"\\"a": "1"
+					}`,
+					toBe: { '"a': '1' },
+				},
+				{
+					desc: 'at key end with string value',
+					expect: `{
+						"a\\"": "1"
+					}`,
+					toBe: { 'a"': '1' },
+				},
+				{
+					desc: 'in string value',
+					expect: `{
+						"a": "\\""
+					}`,
+					toBe: { 'a': '"' },
+				},
+				{
+					desc: 'at value start',
+					expect: `{
+						"a": "\\"1"
+					}`,
+					toBe: { 'a': '"1' },
+				},
+				{
+					desc: 'at value end',
+					expect: `{
+						"a": "1\\""
+					}`,
+					toBe: { 'a': '1"' },
+				},
+			]);
+
+		});
+
+		describe('ignore double quotes', () => {
+
+			runTests([
+				{
+					desc: 'in key and value',
+					expect: `{
+						"\\"": "\\""
+					}`,
+					toBe: { '"': '"' },
+				},
+				{
+					desc: 'at key and value start',
+					expect: `{
+						"\\"a": "\\"1"
+					}`,
+					toBe: { '"a': '"1' },
+				},
+				{
+					desc: 'at key and value end',
+					expect: `{
+						"a\\"": "1\\""
+					}`,
+					toBe: { 'a"': '1"' },
+				},
+			]);
+
+		});
+
+		describe('ignore multiple double quotes', () => {
+
+			runTests([
+				{
+					desc: 'in key and value',
+					expect: `{
+						"\\"\\"\\"": "\\"\\"\\""
+					}`,
+					toBe: { '"""': '"""' },
+				},
+
+				{
+					desc: 'at key and value start',
+					expect: `{
+						"\\"\\"\\"a": "\\"\\"\\"1"
+					}`,
+					toBe: { '"""a': '"""1' },
+				},
+
+				{
+					desc: 'at key and value end',
+					expect: `{
+						"a\\"\\"\\"": "1\\"\\"\\""
+					}`,
+					toBe: { 'a"""': '1"""' },
+				},
+			]);
+
+		});
+
 	});
-	
+
 });
