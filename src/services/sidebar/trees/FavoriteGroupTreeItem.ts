2988524a542a498e9775cc72edd77972cc8fe939
L13 <L13.RARY@gmail.com>
2021-02-26 20:44:32 +0100

    - removed state fix

--- src/services/sidebar/trees/FavoriteGroupTreeItem.ts
+++ src/services/sidebar/trees/FavoriteGroupTreeItem.ts
@@ -16,22 +16,13 @@ import { FavoriteGroup } from '../../../types';
 
 export class FavoriteGroupTreeItem extends vscode.TreeItem {
 	
-	// triggers the tree view to recreate all items for collapse all
-	private static stateVersion:number = 0;
-	
-	public static updateStateVersion () {
-		
-		FavoriteGroupTreeItem.stateVersion = 1 - FavoriteGroupTreeItem.stateVersion;
-		
-	}
-	
 	public contextValue = 'favoriteGroup';
 	
 	public constructor (public readonly favoriteGroup:FavoriteGroup) {
 		
 		super(favoriteGroup.label, favoriteGroup.collapsed ? vscode.TreeItemCollapsibleState.Collapsed : vscode.TreeItemCollapsibleState.Expanded);
 		
-		this.id = `${favoriteGroup.id}-${FavoriteGroupTreeItem.stateVersion}`;
+		this.id = `${favoriteGroup.id}`;
 		
 	}
 	
